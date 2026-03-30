67f14da5f8738d31610f52bd1a4cb2758d3f00da
alshatreee <83844901+alshatreee@users.noreply.github.com>
2026-03-30 12:37:49 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,39 @@
+import { withSentryConfig } from '@sentry/nextjs'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-      },
-      {
-        protocol: 'https',
-        hostname: 'img.clerk.com',
-      },
-    ],
-  },
+    images: {
+          remotePatterns: [
+            {
+                      protocol: 'https',
+                      hostname: 'res.cloudinary.com',
+            },
+            {
+                      protocol: 'https',
+                      hostname: 'picsum.photos',
+            },
+            {
+                      protocol: 'https',
+                      hostname: 'img.clerk.com',
+            },
+                ],
+    },
 }
 
-export default nextConfig
+export default withSentryConfig(nextConfig, {
+    // Sentry org and project from environment variables
+                                  org: process.env.SENTRY_ORG,
+    project: process.env.SENTRY_PROJECT,
+
+    // Only print logs for uploading source maps in CI
+    silent: !process.env.CI,
+
+    // Upload larger set of source maps for prettier stack traces
+    widenClientFileUpload: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+})
